d3d275cfa1d2ffa75757a0238adfcdcdbb62dc3a
KhaVNguyen <kha.vng@gmail.com>
2019-09-25 10:15:41 -0700

    Remove extra files

---
+++ src/Components/Test.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { Activate } from '../activate'
+
+export default function Test() {
+    useEffect(() => {
+
+    }, [])
+
+    return (
+       
+    );
+}
